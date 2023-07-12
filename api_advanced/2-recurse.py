658c35e9784ed20502a0e96fb4d8fc482e49d1f2
Mgeni-E <e.mgeni@alustudent.com>
2023-07-12 12:32:27 -0700

    Initial commit

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,26 +1,44 @@
 #!/usr/bin/python3
-"""docs"""
+"""
+Function that queries the Reddit API and prints
+the top ten hot posts of a subreddit
+"""
 import requests
+import sys
+
+
+def add_title(hot_list, hot_posts):
+    if len(hot_posts) == 0:
+        return
+    hot_list.append(hot_posts[0]['data']['title'])
+    hot_posts.pop(0)
+    add_title(hot_list, hot_posts)
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """"Doc"""
-    url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    header = {'User-Agent': 'Mozilla/5.0'}
-    param = {'after': after}
-    resopnse = requests.get(url, headers=header, params=param)
+    agent = 'Mozilla/5.0'
+    headers = {
+        'User-Agent': agent
+    }
 
-    if resopnse.status_code != 200:
+    params = {
+        'after': after
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
+
+    if response.status_code != 200:
         return None
-    else:
-        json_res = resopnse.json()
-        after = json_res.get('data').get('after')
-        has_next = \
-            json_res.get('data').get('after') is not None
-        hot_articles = json_res.get('data').get('children')
-        [hot_list.append(article.get('data').get('title'))
-         for article in hot_articles]
 
-        return recurse(subreddit, hot_list, after=after) \
-            if has_next else hot_list
+    reddit = response.json()
+    hot_posts = reddit['data']['children']
+    add_title(hot_list, hot_posts)
+    after = reddit['data']['after']
+    if not after:
+        return hot_list
+    else:
+        return recurse(subreddit, hot_list=hot_list, after=after)
